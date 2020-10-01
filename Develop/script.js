b5ea3f9f7268a0b2834a066db8f4272dcc01da96
Dylan Lucarelli <dyllucar@gmail.com>
2020-09-30 20:14:16 -0500

    Adds initial html, css and js files

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+var list = document.querySelector(".container")
+var date = document.querySelector("#currentDay")
+
+
+//create a list of blocked hours from 6am-5pm
+//create a corresponding text block for the events to be written in
+//create a corresponding save button that saves the text to local storage
+//when the page loads it should pull the saved text from local storage to populate the text boxes
+//add current date to the top of the page that updates based on actual date
+//attach class color to the hours past, present and future
